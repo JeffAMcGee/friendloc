b4bbbed914245123cf14f0940d91a62f2390d7b8
Jeff McGee <JeffAMcGee@gmail.com>
2011-05-18 19:23:21 -0500

    removed from eval users without rels

--- predict/eval.py
+++ predict/eval.py
@@ -14,28 +14,30 @@ def eval_block(*args):
 
     users = read_json("samp/eval"+block)
     dists = defaultdict(list)
+    skipped=0
 
     for user in users:
         mloc = user['mloc']
+        if not user['rels']:
+            skipped+=1
+            continue
         if 'gnp' not in user or user['gnp']['code']=="COORD":
             user['gnp'] = settings.default_loc
         for predictor in preds:
             res = globals()['pred_'+predictor](user)
             dists[predictor].append(coord_in_miles(mloc, res))
     write_json([dists],"samp/res"+block)
+    print skipped
 
 
 def pred_median(user):
-    if user['rels']:
-        return median_2d((r['lng'],r['lat']) for r in user['rels'])
-    else:
-        return settings.default_loc
+    return median_2d((r['lng'],r['lat']) for r in user['rels'])
+
 
 def pred_geocoding(user):
     return user['gnp']
 
+
 def pred_omniscient(user):
-    if not user['rels']:
-        return user['gnp']
     return min(user['rels'], key=lambda rel: coord_in_miles(user['mloc'],rel))
     
