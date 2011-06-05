0641d620a6d2cc5e6043e8f41cda8bf13e4128ac
Jeff McGee <JeffAMcGee@gmail.com>
2011-06-04 22:57:45 -0500

    removing old junk from codebase

--- localcrawl/admin.py
+++ localcrawl/admin.py
@@ -14,20 +14,16 @@ import pymongo
 import restkit.errors
 try:
     import beanstalkc
-    from couchdbkit import ResourceNotFound, BulkSaveError
-    from couchdbkit.loaders import FileSystemDocsLoader
 except:
     pass
 
 from settings import settings
-from scoredict import Scores, BUCKETS, log_score, DONE
 from localcrawl.crawl import CrawlProcess
 import localcrawl.lookup as lookup
 from localcrawl.procs import create_slaves
-import base.twitter
 from base.models import *
 from maroon import Model
-from base.utils import grouper, couch, mongo, in_local_box, read_json
+from base.utils import grouper, in_local_box, read_json
 
 
 def localcrawl():
@@ -44,12 +40,6 @@ def crawl_once(region):
             slaves=settings.slaves,
             log_level=logging.INFO)
     proc.run()
-
-
-def design_sync(type):
-    "sync the documents in _design"
-    loader = FileSystemDocsLoader(type+'_design')
-    loader.sync(Model.database, verbose=True)
 
 
 def start_lookup():
@@ -88,29 +78,6 @@ def import_json():
                 logging.warn("conflicts for %r",[d['id'] for d in err.errors])
 
 
-def filter_usians(path=None):
-    file =open(path) if path else sys.stdin 
-    for line in file:
-        tweet = cjson.decode(line)
-        if not tweet.get('coordinates'):
-            continue
-        lng,lat = tweet['coordinates']['coordinates']
-        if 24<lat<50 and -126<lng<-66:
-            print line,
-
-def import_old_json():
-    for g in grouper(1000,sys.stdin):
-        docs = [json.loads(l) for l in g if l]
-        for d in docs:
-            del d['doc_type']
-            for k,v in d.iteritems():
-                if k[-2:]=='id' or k in ('rtt','rtu'):
-                    d[k]=v[1:]
-            for field in ['ats','fols','frs']:
-                if field in d and isinstance(d[field],list):
-                    d[field] = [u[1:] for u in d[field]]
-        Model.database.bulk_save(docs)
-
 def make_indexes(host=settings.mongo_host):
     keys = [('User','ncd'),
             ('Tweet',[('uid',1),('ca',1)]),
@@ -121,27 +88,6 @@ def make_indexes(host=settings.mongo_host):
             connection[db][key[0]].create_index(key[1])
 
 
-def export_json(start=None,end=None):
-    for d in Model.database.paged_view('_all_docs',include_docs=True,startkey=start,endkey=end):
-        if d['id'][0]!='_':
-            del d['doc']['_rev']
-            print json.dumps(d['doc'])
- 
-
-def export_mongo():
-    for d in Model.database.paged_view('_all_docs',include_docs=True):
-        if d['id'][0]!='_':
-            d = d['doc']
-            del d['_rev']
-            for k,v in d.iteritems():
-                if k[-2:]=='id' or k in ('rtt','rtu'):
-                    d[k]=int(v)
-            for field in ['ats','fols','frs']:
-                if field in d and isinstance(d[field],list):
-                    d[field] = [int(u) for u in d[field][:5000]]
-            print json.dumps(d)
-
-
 def import_mongo(cls_name,path=None):
     Cls = globals()[cls_name]
     for g in grouper(1000,read_json(path)):
@@ -149,19 +95,13 @@ def import_mongo(cls_name,path=None):
 
 
 def fix_crowd_uids():
+    #FIXME: this is for crowdy
     for crowd in Model.database.Crowd.find():
         for k in ['start','end']:
             crowd[k] = dt.fromtimestamp(crowd[k])# if crowd[k] else dt(2010,12,28)
         for user in crowd['users']:
             user['id'] = int(user['id'])
         Model.database.Crowd.save(crowd)
-
-def fix_jats():
-    settings.pdb()
-    for user in Model.database.User.find({'jat':{'$exists':1}}):
-        user['jats'] = user['jat']
-        del user['jat']
-        Model.database.User.save(user)
 
 
 def pick_locals(path=None):
@@ -173,164 +113,3 @@ def pick_locals(path=None):
         d = dict(zip(['lng','lat'],tweet['coordinates']['coordinates']))
         if in_local_box(d):
             print json.dumps(tweet['user'])
-
-
-def merge_db(*names,**kwargs):
-    views = [
-        connect(name).paged_view(
-            '_all_docs',
-            include_docs=True,
-            startkey=kwargs.get('start'),
-            endkey=kwargs.get('end'))
-        for name in names
-    ]
-    last =None
-    for row in merge_views(*views):
-        if row['key']!=last:
-            del row['doc']['_rev']
-            print json.dumps(row['doc'])
-            last = row['key']
-
-
-def fake_lu_master():
-    proc = lookup.LookupMaster()
-    while not proc.halt:
-        proc.read_scores()
-        print "scores:%d"%len(proc.scores)
-    print "halting"
-    proc.read_scores()
-    proc.scores.dump(settings.lookup_out)
-
-
-def fake_lu_slave():
-    proc = lookup.LookupSlave('y')
-    Edges.database = CouchDB('http://127.0.0.1:5984/orig_houtx',True)
-    view = Model.database.paged_view('dev/user_and_tweets',include_docs=True)
-    for k,g in itertools.groupby(view, lambda r:r['key'][0]):
-        user_d = g.next()
-        if user_d['id'][0] != 'U':
-            print "fail %r"%user_d
-            continue
-        user = User(user_d['doc'])
-        print "scoring %s - %s"%(user._id, user.screen_name)
-        tweets = [Tweet(r['doc']) for r in g]
-        if user.local_prob != 1.0:
-            continue
-        try:
-            rels = Edges.get_for_user_id(user._id)
-        except ResourceNotFound:
-            print "rels not found"
-            rels = None
-        proc.score_new_users(user, rels, tweets)
-    print "done"
-
-
-def update_loc():
-    User.database = connect("houtx_user")
-    for user in User.get_all():
-        old = user.local_prob
-        user.local_prob = lookup.guess_location(user,gisgraphy)
-        if old != user.local_prob:
-            user.save()
-
-
-def merge_views(*views):
-    # This is based on heapq.merge in python 2.6.  The big difference is
-    # that it sorts by key.
-    h = []
-    for itnum, it in enumerate(map(iter, views)):
-        try:
-            row = it.next()
-            h.append([row['key'], itnum, row, it.next])
-        except StopIteration:
-            pass
-    heapq.heapify(h)
-
-    while 1:
-        try:
-            while 1:
-                k, itnum, v, next = s = h[0]   # raises IndexError when h is empty
-                yield v
-                s[2] = next()               # raises StopIteration when exhausted
-                s[0] = s[2]['key']
-                heapq.heapreplace(h, s)          # restore heap condition
-        except StopIteration:
-            heapq.heappop(h)                     # remove empty iterator
-        except IndexError:
-            return
-
-
-def _users_from_scores():
-    scores = Scores()
-    scores.read(settings.lookup_out)
-    for uid in scores:
-        state, rfs, ats = scores.split(uid)
-        if log_score(rfs,ats)>=11:
-            yield uid
-
-
-def _users_from_db():
-    User.database = connect("houtx_user")
-    return (int(row['id']) for row in User.database.paged_view("_all_docs"))
-    
-
-def fetch_edges():
-    Edges.database = connect("houtx_edges")
-    User.database = connect("away_user")
-    old_edges = set(int(row['id']) for row in Edges.database.paged_view("_all_docs",endkey="_"))
-    uids = set(_users_from_scores())-old_edges
-    for g in grouper(100,uids):
-        for user in twitter.user_lookup(g):
-            if user is None or user.protected: continue
-            try:
-                edges = twitter.get_edges(user._id)
-            except restkit.errors.Unauthorized:
-                logging.warn("unauthorized!")
-                continue
-            except restkit.errors.ResourceNotFound:
-                logging.warn("resource not found!?")
-                continue
-            edges.save()
-            user.save()
-            twitter.sleep_if_needed()
-
-
-def stdin_lookup():
-    from_db = connect("orig_houtx")
-    for l in sys.stdin:
-        user = from_db.get_id(User,l.strip())
-        user_lookup(user)
-
-
-def user_lookup(user):
-    tweets = twitter.save_timeline(user._id,last_tid=settings.min_tweet_id)
-    if not tweets: return
-    user.last_tid = tweets[0]._id
-    user.last_crawl_date = dt.utcnow()
-    user.next_crawl_date = dt.utcnow()
-    user.tweets_per_hour = settings.tweets_per_hour
-    user.lookup_done = True
-    user.attempt_save()
-    logging.info("saved %d from %s to %s",len(tweets),tweets[-1]._id,tweets[0]._id)
-    twitter.sleep_if_needed()
-
-
-def update_mongo():
-    for user in User.get_all():
-        try:                                                                             
-            user.save()
-        except:                                                                          
-            pdb.post_mortem()    
-
-def fill_50():
-    Tweet.database = couch('hou_new_tweet')
-    old_db = couch('houtx_tweet')
-    res = twitter.TwitterResource()
-    for line in open('logs/missing_uids'):
-        uid = line.strip()
-        view = old_db.paged_view('tweet/uid',key=uid)
-        last = max(int(row['id']) for row in view)
-        tweets = res.save_timeline(uid, last_tid=last)
-        logging.info("saved %d for %s",len(tweets),uid)
-        twitter.sleep_if_needed()
-
