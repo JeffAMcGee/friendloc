508f67e94da4bcaba39e7827f7b7420e942dc3f1
Jeff McGee <JeffAMcGee@gmail.com>
2012-06-28 00:48:48 -0500

    fixed mloc_uids

--- explore/sprawl.py
+++ explore/sprawl.py
@@ -49,17 +49,6 @@ def parse_geotweets(tweets):
 
 
 @gob.mapper(all_items=True)
-def mloc_uids(user_ds):
-    retrieved = [u['id'] for u in itertools.islice(user_ds,2600)]
-    # this query is probably painful.
-    users = User.find(User._id.is_in(retrieved) and User.neighbors.exists())
-    good_ = { u._id for u in users }
-    good = [uid for uid in retrieved if uid in good_]
-    # throw away accounts that didn't work to get down to the 2500 good users
-    return good[:2500]
-
-
-@gob.mapper(all_items=True)
 def mloc_users(users_and_coords):
     users = {}
     locs = defaultdict(list)
@@ -148,6 +137,18 @@ class EdgeFinder(Sprawler):
         user = User.get_id(uid)
         self._save_user_contacts(user,limit=25)
         return self._my_nebrs(user)
+
+
+@gob.mapper(all_items=True)
+def mloc_uids(user_ds):
+    retrieved = [u['id'] for u in itertools.islice(user_ds,2600)]
+    # this query is probably painful.
+    users = User.find(User._id.is_in(retrieved))
+    good_ = { u._id for u in users if any(getattr(u,k) for k in NEBR_KEYS)}
+    good = [uid for uid in retrieved if uid in good_]
+    logging.info("found %d of %d",len(good),len(retrieved))
+    # throw away accounts that didn't work to get down to the 2500 good users
+    return good[:2500]
 
 
 @gob.mapper(all_items=True)
