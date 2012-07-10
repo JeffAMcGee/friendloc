126733a6f9f0975e9c5f65e6841ebba1033ad460
Jeff McGee <JeffAMcGee@gmail.com>
2012-07-09 20:45:56 -0500

    working TreeRegressor

--- predict/fl.py
+++ predict/fl.py
@@ -30,20 +30,21 @@ def nebr_vect(user):
 def _transformed(vects):
     # convert vects to a scaled numpy array
     vects_ = np.fromiter( chain.from_iterable(vects), int )
-    vects_.shape = (len(vects_)//8),8
+    vects_.shape = (len(vects_)//10),10
     X = np.array(vects_[:,:-1],float)
     y = np.array(vects_[:,-1],int)
-    scaler = preprocessing.Scaler().fit(X)
+    #scaler = preprocessing.Scaler().fit(X)
 
     # In final system, you will want to only fit the training data.
-    return scaler.transform(X), y
+    #return scaler.transform(X), y
+    return X,y
 
 
 @gob.mapper(all_items=True)
 def fl_learn(vects):
     #./gb.py -i edge_vect.22 fl_learn
     X, y = _transformed(vects)
-    clf = tree.DecisionTreeClassifier()
+    clf = tree.DecisionTreeRegressor(max_depth=8)
     scores = cross_validation.cross_val_score(clf,X,y,cv=5)
     print "Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() / 2)
     return []
