a27bf6c91c0036ab6eedf1b9da38e84dd91851e0
Jeff McGee <JeffAMcGee@gmail.com>
2013-01-18 12:49:55 -0600

    add location information to weak_edges crowds

--- crowds/crowds.py
+++ crowds/crowds.py
@@ -109,14 +109,15 @@ def mcl_edges(edges):
         if len(uids)>1:
             yield [int(uid) for uid in uids]
 
-@gob.mapper(all_items=True)
-def weak_edges(edges):
+@gob.mapper(all_items=True,slurp={'user_locs':dict})
+def weak_edges(edges,user_locs):
     ats = nx.DiGraph()
     for frm,to,dist in edges:
         if dist>.5:
             ats.add_edge(frm,to)
-    scc = nx.weakly_connected_components(ats)
-    return (c for c in scc if len(c)>1)
-
-
-
+    wcc = nx.weakly_connected_components(ats)
+    return (
+            [(id,user_locs[id]) for id in clust]
+            for clust in wcc
+            if len(clust)>2
+        )
